2d5e1eb398a40b6f3a97909efe9fb111ba36c244
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-11-08 22:47:36 -0600

    Update: Verlet Integration

--- project/verlet_integration.py
+++ project/verlet_integration.py
@@ -263,3 +263,302 @@ class EulerIntegrationError(Scene):
         )
 
         self.wait(1)
+
+
+class VerletIntegration(Scene):
+    def construct(self) -> None:
+        # Calculate the position at t + dt.
+        pos_t_dt = Tex(r"f(t + \Delta t) = f(t) + \int_{t}^{t + \Delta t}f'(x) dx", isolate=[
+            "f", "(", "x", ")", "\int", "dx"])
+
+        self.play(Write(pos_t_dt))
+
+        self.wait(1)
+
+        int_t_t_dt = Tex(r"\int_{t}^{t + \Delta t}f'(x) dx", isolate=[
+            "f", "(", "x", ")", "\int", "dx"
+        ])
+
+        self.play(TransformMatchingTex(pos_t_dt, int_t_t_dt))
+
+        self.wait(1)
+
+        u_tex = Tex(r"u = f'(x)", isolate=["u", "f", "(", "x", ")"])
+        dv_tex = Tex(r"dv = dx", isolate=["v", "dx"])
+
+        u_dv_group = VGroup(u_tex, dv_tex)
+        u_dv_group.arrange(RIGHT, buff=0.1)
+        u_tex.shift(0.5*LEFT)
+        dv_tex.shift(0.5*RIGHT)
+
+        du_tex = Tex(r"du = f''(x) dx", isolate=["u", "f", "(", "x", ")"])
+        v_tex = Tex(r"v = x + C", isolate=["v", "x"])
+
+        du_v_group = VGroup(du_tex, v_tex)
+        du_v_group.arrange(RIGHT, buff=0.1)
+        du_tex.shift(0.5*LEFT)
+        v_tex.shift(0.5*RIGHT)
+        du_v_group.next_to(u_dv_group, DOWN)
+
+        # Move the integral to the up.
+        # Write u and dv.
+
+        self.play(
+            int_t_t_dt.animate.shift(UP*1.5)
+        )
+        self.play(
+            Write(u_tex),
+            Write(dv_tex)
+        )
+
+        # Write du and v.
+        self.play(
+            TransformMatchingTex(u_tex.copy(), du_tex),
+            TransformMatchingTex(dv_tex.copy(), v_tex)
+        )
+
+        # int_t_t_dt by parts.
+        int_t_t_dt_parts = Tex(
+            r"""
+            \int_{t}^{t + \Delta t}f'(x) dt = 
+            ((x + C)f'(x)) \vert_{t}^{t + \Delta t} - 
+            \int_{t}^{t + \Delta t} (x + C)f''(x) dx
+            """,
+            isolate=[
+                "f", "(", "x", ")", "\int", "dx", "C"
+            ]
+        )
+        int_t_t_dt_parts.next_to(int_t_t_dt, ORIGIN)
+
+        self.play(
+            TransformMatchingTex(int_t_t_dt, int_t_t_dt_parts)
+        )
+
+        self.wait(1)
+
+        # Choose C = -(t + dt).
+        c_tex = Tex(r"C = -(t + \Delta t)", isolate=["C", "t", "\Delta t"])
+        c_tex.next_to(du_v_group, DOWN)
+
+        self.play(
+            Write(c_tex)
+        )
+
+        self.wait(1)
+
+        # Replace C.
+        v_tex_new = Tex(r"v = x - (t + \Delta t)",
+                        isolate=["v", "x", "t", "\Delta t"])
+        v_tex_new.next_to(v_tex, ORIGIN)
+        int_t_t_dt_parts_new = Tex(
+            r"""
+            \int_{t}^{t + \Delta t}f'(x) dt = 
+            ((x - (t + \Delta t))f'(x)) \vert_{t}^{t + \Delta t} - 
+            \int_{t}^{t + \Delta t} (x - (t + \Delta t))f''(x) dx
+            """,
+            isolate=[
+                "f", "(", "x", ")", "\int", "dx", "C"
+            ]
+        )
+        int_t_t_dt_parts_new.next_to(int_t_t_dt_parts, ORIGIN)
+
+        self.play(
+            TransformMatchingTex(v_tex, v_tex_new),
+            TransformMatchingTex(int_t_t_dt_parts, int_t_t_dt_parts_new)
+        )
+
+        self.wait(1)
+
+        # Fade out the u, dv, du, v, and C.
+        self.play(
+            FadeOut(u_tex),
+            FadeOut(dv_tex),
+            FadeOut(du_tex),
+            FadeOut(v_tex_new),
+            FadeOut(c_tex)
+        )
+
+        self.wait(1)
+
+        # When x = t + dt.
+        prompt, tex = group = VGroup(
+            Tex(r"When\ x = t + \Delta t \ ,"),
+            Tex(r"(x - (t + \Delta t))f'(x) = 0", isolate=[
+                "x", "t", "\Delta t", "f", "(", ")", "0"
+            ])
+        )
+        group.arrange(RIGHT, buff=0.1)
+        prompt.shift(LEFT*0.5)
+
+        self.play(
+            Write(prompt),
+            Write(tex)
+        )
+
+        self.wait(1)
+
+        # Expand ((x - (t + \Delta t))f'(x)) \vert_{t}^{t + \Delta t}
+
+        l1, l2, l3 = expanded_tex = VGroup(
+            Tex(
+                r"""
+                ((x - (t + \Delta t))f'(x)) \vert_{t}^{t + \Delta t} 
+                = 0 - ((t - (t + \Delta t))f'(t))
+                """
+            ),
+            Tex(
+                r"""
+                = 0 - (- \Delta t f'(t))
+                """,
+                isolate=[
+                    "t", "\Delta t", "f", "(", ")", "0"
+                ]
+            ),
+            Tex(
+                r"""
+                = \Delta t f'(t)
+                """,
+                isolate=[
+                    "t", "\Delta t", "f", "(", ")"
+                ]
+            ),
+        )
+
+        expanded_tex.arrange(DOWN, buff=0.1)
+        expanded_tex.next_to(group, ORIGIN)
+        expanded_tex.shift(DOWN*0.5)
+
+        l2.shift(1.83*RIGHT)
+        l3.shift(1.05*RIGHT)
+
+        self.play(FadeOut(prompt))
+        self.play(TransformMatchingTex(tex, expanded_tex))
+
+        self.wait(1)
+
+        # int_t_t_dt_parts new
+        int_t_t_dt_parts_new2 = Tex(
+            r"""
+            \int_{t}^{t + \Delta t}f'(x) dt = 
+            \Delta t f'(t) - 
+            \int_{t}^{t + \Delta t} (x - (t + \Delta t))f''(x) dx
+            """,
+            isolate=[
+                "f", "(", "x", ")", "\int", "dx", "C"
+            ]
+        )
+
+        int_t_t_dt_parts_new2.next_to(int_t_t_dt_parts, ORIGIN)
+
+        self.play(
+            TransformMatchingTex(int_t_t_dt_parts_new, int_t_t_dt_parts_new2)
+        )
+
+        self.wait(1)
+
+        # Bring back pos_t_dt.
+        self.play(
+            FadeOut(expanded_tex)
+        )
+        self.play(
+            Write(pos_t_dt)
+        )
+
+        # Expand pos_t_dt.
+        expanded_tex = Tex(
+            r"""
+            f(t + \Delta t) = f(t) +
+            \Delta t f'(t) - 
+            \int_{t}^{t + \Delta t} (x - (t + \Delta t))f''(x) dx
+            """,
+            isolate=[
+                "f", "(", "x", ")", "\int", "dx", "C"
+            ]
+        )
+
+        self.play(
+            TransformMatchingTex(pos_t_dt, expanded_tex),
+            TransformMatchingTex(int_t_t_dt_parts_new2, expanded_tex),
+        )
+
+        self.wait(1)
+
+        # Expand the pos_t_dt 2.
+        f_t_plus_dt = Tex(
+            r"f(t", r"+", r"\Delta t)", r"=", r"f(t)", r"+", r"\Delta t f'(t)", r"+",
+            r"\Delta t^2", r"{f''(t)", r"\over", r"2!}", r"+",
+            r"\Delta t^3", r"{f^{(3)}(t)", r"\over", r"3!}", r"+",
+            r"\cdots"
+        )
+
+        self.play(
+            TransformMatchingTex(expanded_tex, f_t_plus_dt),
+        )
+
+        self.wait(1)
+
+        # Expand f_t_minus_dt.
+        f_t_minus_dt = Tex(
+            r"f(t", r"-", r"\Delta t)", r"=", r"f(t)", r"-", r"\Delta t f'(t)", r"+",
+            r"\Delta t^2", r"{f''(t)", r"\over", r"2!}", r"-",
+            r"\Delta t^3", r"{f^{(3)}(t)", r"\over", r"3!}", r"+",
+            r"\cdots"
+        )
+
+        self.play(
+            f_t_plus_dt.animate.shift(UP*0.75),
+        )
+
+        f_t_minus_dt.next_to(f_t_plus_dt, DOWN*1.5)
+
+        self.play(
+            TransformMatchingTex(f_t_plus_dt.copy(), f_t_minus_dt),
+        )
+
+        self.wait(1)
+
+        # Trow away latter terms.
+        f_t_plus_dt_approx = Tex(
+            r"f(t", r"+", r"\Delta t) \approx f(t)", r"+", r"\Delta t f'(t)", r"+",
+            r"\Delta t^2", r"{f''(t)", r"\over", r"2!}", r"+",
+            r"\Delta t^3", r"{f^{(3)}(t)", r"\over", r"3!}"
+        )
+        f_t_plus_dt_approx.next_to(f_t_plus_dt, ORIGIN)
+        f_t_minus_dt_approx = Tex(
+            r"f(t", r"-", r"\Delta t) \approx f(t)", r"-", r"\Delta t f'(t)", r"+",
+            r"\Delta t^2", r"{f''(t)", r"\over", r"2!}", r"-",
+            r"\Delta t^3", r"{f^{(3)}(t)", r"\over", r"3!}"
+        )
+        f_t_minus_dt_approx.next_to(f_t_minus_dt, ORIGIN)
+
+        self.play(
+            TransformMatchingTex(f_t_plus_dt, f_t_plus_dt_approx),
+            TransformMatchingTex(f_t_minus_dt, f_t_minus_dt_approx),
+        )
+
+        self.wait(1)
+
+        # Combine together.
+
+        combine = Tex(
+            r"f(t + \Delta t)", r"+", r"f(t - \Delta t) \approx 2f(t)", r"+",
+            r"2", r"\Delta t^2", r"{f''(t)", r"\over", r"2!}",
+        )
+
+        self.play(
+            TransformMatchingTex(f_t_plus_dt_approx, combine),
+            TransformMatchingTex(f_t_minus_dt_approx, combine),
+        )
+
+        self.wait(1)
+
+        # Final result.
+        final_result = Tex(
+            r"f(t + \Delta t) \approx 2f(t)", r"-",
+            r"f(t - \Delta t)", r"+",
+            r"\Delta t^2", r"f''(t)",
+        )
+
+        self.play(
+            TransformMatchingTex(combine, final_result),
+        )
